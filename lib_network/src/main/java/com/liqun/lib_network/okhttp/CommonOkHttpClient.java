bedf123306ab191857b3812d41ded7fcecf57bd2
MoeRookie <MoeRookie@163.com>
2020-09-06 23:25:12 +0800

    封装用于连接请求及响应的枢纽类CommonOkHttpClient

---
+++ lib_network/src/main/java/com/liqun/lib_network/okhttp/CommonOkHttpClient.java
@@ -0,0 +1,87 @@
+package com.liqun.lib_network.okhttp;
+
+import com.liqun.lib_network.okhttp.listener.DisposeDataHandle;
+import com.liqun.lib_network.okhttp.response.CommonFileCallback;
+import com.liqun.lib_network.okhttp.response.CommonJsonCallback;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+
+import okhttp3.Call;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * 用来发送get, post请求的工具类，包括设置一些请求的共用参数
+ */
+public class CommonOkHttpClient {
+    private static final int TIME_OUT = 30;
+    private static OkHttpClient mOkHttpClient;
+    static {
+        OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder();
+        clientBuilder.hostnameVerifier(new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                return true; // 允许所有客户端访问此域名
+            }
+        });
+        /**
+         * 为所有请求添加请求头, 看个人需求
+         */
+        clientBuilder.addInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request request =
+                        chain.request().newBuilder()
+                                .addHeader("User-Agent", "Imooc-Mobile") // 标明发送本次请求的客户端
+                                .build();
+                return chain.proceed(request);
+            }
+        });
+        clientBuilder.connectTimeout(TIME_OUT, TimeUnit.SECONDS);
+        clientBuilder.readTimeout(TIME_OUT, TimeUnit.SECONDS);
+        clientBuilder.writeTimeout(TIME_OUT, TimeUnit.SECONDS);
+        clientBuilder.followRedirects(true); // 重定向
+        mOkHttpClient = clientBuilder.build();
+    }
+
+    /**
+     * 通过构造好的Request, Callback去发送请求(Get请求)
+     * @param handle
+     * @return
+     */
+    public static Call get(Request request, DisposeDataHandle handle){
+        Call call = mOkHttpClient.newCall(request);
+        call.enqueue(new CommonJsonCallback(handle));
+        return call;
+    }
+
+    /**
+     * post请求
+     * @param request
+     * @param handle
+     * @return
+     */
+    public static Call post(Request request, DisposeDataHandle handle){
+        Call call = mOkHttpClient.newCall(request);
+        call.enqueue(new CommonJsonCallback(handle));
+        return call;
+    }
+
+    /**
+     * 文件下载请求
+     * @param request
+     * @param handle
+     * @return
+     */
+    public static Call downloadFile(Request request, DisposeDataHandle handle){
+        Call call = mOkHttpClient.newCall(request);
+        call.enqueue(new CommonFileCallback(handle));
+        return call;
+    }
+}
