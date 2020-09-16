287bbbc44d2838c0bd46279a38e88ad8dbc2129c
MoeRookie <MoeRookie@163.com>
2020-09-16 18:42:03 +0800

    自定义mediaPlayer以对外提供当前状态

---
+++ lib_audio/src/main/java/com/liqun/lib_audio/mediaplayer/core/CustomMediaPlayer.java
@@ -0,0 +1,73 @@
+package com.liqun.lib_audio.mediaplayer.core;
+
+import android.media.MediaPlayer;
+
+import java.io.IOException;
+
+/**
+ * 带状态的MediaPlayer
+ */
+public class CustomMediaPlayer extends MediaPlayer implements MediaPlayer.OnCompletionListener {
+
+    public enum Status{
+        IDLE, INITIALIZED, STARTED, PAUSED, STOPPED, COMPLETED
+    }
+
+    private Status mState;
+
+    public CustomMediaPlayer() {
+        super();
+        mState = Status.IDLE;
+        super.setOnCompletionListener(this);
+    }
+
+    @Override
+    public void reset() {
+        super.reset();
+        mState = Status.IDLE;
+    }
+
+    @Override
+    public void setDataSource(String path) throws IOException, IllegalArgumentException, IllegalStateException, SecurityException {
+        super.setDataSource(path);
+        mState = Status.INITIALIZED;
+    }
+
+    @Override
+    public void start() throws IllegalStateException {
+        super.start();
+        mState = Status.STARTED;
+    }
+
+    @Override
+    public void pause() throws IllegalStateException {
+        super.pause();
+        mState = Status.PAUSED;
+    }
+
+    @Override
+    public void stop() throws IllegalStateException {
+        super.stop();
+        mState = Status.STOPPED;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mediaPlayer) {
+        mState = Status.COMPLETED;
+    }
+
+    public Status getState(){
+        return mState;
+    }
+
+    public boolean isComplete(){
+        return mState == Status.COMPLETED;
+    }
+
+    private OnCompletionListener mOnCompletionListener;
+
+    public void setOnCompletionListener(OnCompletionListener listener){
+        mOnCompletionListener = listener;
+    }
+
+}
