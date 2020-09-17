4e8cc8d37904df99073ff7b5280ad2ee4328d613
MoeRookie <MoeRookie@163.com>
2020-09-17 15:36:52 +0800

    对外提供加载、播放、暂停、恢复以及销毁等方法

--- lib_audio/src/main/java/com/liqun/lib_audio/mediaplayer/core/AudioPlayer.java
+++ lib_audio/src/main/java/com/liqun/lib_audio/mediaplayer/core/AudioPlayer.java
@@ -8,8 +8,10 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.os.PowerManager;
+import android.util.Log;
 
 import com.liqun.lib_audio.mediaplayer.app.AudioHelper;
+import com.liqun.lib_audio.mediaplayer.model.AudioBean;
 
 /**
  * 1、播放音频
@@ -54,6 +56,92 @@ public class AudioPlayer implements MediaPlayer.OnCompletionListener,
         mWifiLock = ((WifiManager) AudioHelper.getContext().getApplicationContext()
                 .getSystemService(Context.WIFI_SERVICE)).createWifiLock(WifiManager.WIFI_MODE_FULL, TAG);
         mAudioFocusManager = new AudioFocusManager(AudioHelper.getContext(), this);
+    }
+
+    /**
+     * 对外提供加载
+     * @param audioBean
+     */
+    public void load(AudioBean audioBean){
+        try{ // 正常加载逻辑
+            mMediaPlayer.reset();
+            mMediaPlayer.setDataSource(audioBean.mUrl);
+            mMediaPlayer.prepareAsync();
+            // 对外发送load事件
+        }catch (Exception e){
+            // 对外发送error事件
+        }
+    }
+
+    /**
+     * 内部开始播放
+     */
+    private void start(){
+        if (!mAudioFocusManager.requestAudioFocus()) {
+            Log.e(TAG, "获取音频焦点失败");
+        }
+        mMediaPlayer.start();
+        mWifiLock.acquire();
+        // 对外发送start事件
+    }
+
+    /**
+     * 对外提供暂停方法
+     */
+    public void pause(){
+        if (getStatus() == CustomMediaPlayer.Status.STARTED) {
+            mMediaPlayer.pause();
+            // 释放音频焦点WifiLock
+            if (mWifiLock.isHeld()) {
+                mWifiLock.release();
+            }
+            // 释放音频焦点
+            if (mAudioFocusManager != null) {
+                mAudioFocusManager.abandonAudioFocus();
+            }
+            // 发送暂停事件
+        }
+    }
+
+    /**
+     * 对外提供恢复方法
+     */
+    public void resume(){
+        if (getStatus() == CustomMediaPlayer.Status.PAUSED) {
+            // 直接复用start
+            start();
+        }
+    }
+
+    /**
+     * 清空播放器占用资源
+     */
+    public void release(){
+        if (mMediaPlayer == null) {
+            return;
+        }
+        mMediaPlayer.release();
+        mMediaPlayer = null;
+        if (mAudioFocusManager != null) {
+            mAudioFocusManager.abandonAudioFocus();
+        }
+        if (mWifiLock.isHeld()) {
+            mWifiLock.release();
+        }
+        mWifiLock = null;
+        mAudioFocusManager = null;
+        // 发送release销毁事件
+    }
+
+    /**
+     * 获取播放器当前状态
+     * @return
+     */
+    public CustomMediaPlayer.Status getStatus(){
+        if (mMediaPlayer != null) {
+            return mMediaPlayer.getState();
+        }
+        return CustomMediaPlayer.Status.STOPPED;
     }
 
     @Override
