ab3c39f455bfc830f1dd980299c97001bdba4980
MoeRookie <MoeRookie@163.com>
2021-01-22 20:13:57 +0800

    初始化音乐后台服务

--- lib_audio/src/main/java/com/liqun/lib_audio/mediaplayer/core/MusicService.java
+++ lib_audio/src/main/java/com/liqun/lib_audio/mediaplayer/core/MusicService.java
@@ -1,12 +1,105 @@
 package com.liqun.lib_audio.mediaplayer.core;
 
-import com.liqun.lib_audio.mediaplayer.app.AudioHelper;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+import android.text.TextUtils;
 
-public class MusicService {
+import com.liqun.lib_audio.mediaplayer.app.AudioHelper;
+import com.liqun.lib_audio.mediaplayer.model.AudioBean;
+import com.liqun.lib_audio.mediaplayer.view.NotificationHelper;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.util.ArrayList;
+
+import androidx.annotation.Nullable;
+
+import static com.liqun.lib_audio.mediaplayer.view.NotificationHelper.NOTIFICATION_ID;
+
+/**
+ * 音乐后台服务
+ * 并更新notification状态
+ */
+public class MusicService extends Service
+implements NotificationHelper.NotificationHelperListener{
+    /**
+     * 常量区
+     */
+    private static String DATA_AUDIOS = "AUDIOS";
+    private static String ACTION_START = "ACTION_START";
+
+    /**
+     * 数据区
+     */
+    private ArrayList<AudioBean> mAudioBeans;
+
+    private NotificationReceiver mReceiver;
+
+    /**
+     * 外部直接启动service的方法
+     * @param audioBeans
+     */
+    public static void startMusicService(ArrayList<AudioBean> audioBeans){
+        Intent intent = new Intent(AudioHelper.getContext(), MusicService.class);
+        intent.setAction(ACTION_START);
+        // 还需要传list数据进来
+        intent.putExtra(DATA_AUDIOS, audioBeans);
+        AudioHelper.getContext().startService(intent);
+    }
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        EventBus.getDefault().register(this);
+        registerBroadcastReceiver();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mAudioBeans = (ArrayList<AudioBean>) intent.getSerializableExtra(DATA_AUDIOS);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
+        unregisterBroadcastReceiver();
+    }
+
+    private void registerBroadcastReceiver() {
+        if (null == mReceiver) {
+            mReceiver = new NotificationReceiver();
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(NotificationReceiver.ACTION_STATUS_BAR);
+            registerReceiver(mReceiver, filter);
+        }
+    }
+
+    private void unregisterBroadcastReceiver() {
+        if (null != mReceiver) {
+            unregisterReceiver(mReceiver);
+        }
+    }
+
+    @Override
+    public void onNotificationInit() {
+        startForeground(NOTIFICATION_ID, NotificationHelper.getInstance().getNotification());
+    }
+
     /**
      * 接收Notification发送的广播
      */
-    public static class NotificationReceiver {
+    public static class NotificationReceiver extends BroadcastReceiver {
         public static final String ACTION_STATUS_BAR =
                 AudioHelper.getContext().getPackageName() + ".NOTIFICATION_ACTIONS";
         public static final String EXTRA = "extra";
@@ -14,5 +107,12 @@ public class MusicService {
         public static final String EXTRA_NEXT = "play_next";
         public static final String EXTRA_PRE = "play_previous";
         public static final String EXTRA_FAV = "play_favourite";
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (null == intent || TextUtils.isEmpty(intent.getAction())) {
+                return;
+            }
+        }
     }
 }
